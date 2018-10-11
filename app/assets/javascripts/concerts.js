3a975aaaf72ffb475c58f9a2b12b26400c07c178
Chris Alberti <christopherjalberti@gmail.com>
2018-10-10 21:45:49 -0400

    refactor appendConcerts to separate function

--- app/assets/javascripts/concerts.js
+++ app/assets/javascripts/concerts.js
@@ -7,21 +7,23 @@ class Concert {
   }
 }
 
-
-
 $(function() {
   addEventListeners();
 });
+
+function appendConcerts(data) {
+  data.forEach(function(concertData) {
+    let concert = new Concert(concertData)
+    $(".display").append(newConcert.title + "<br>")
+  });
+}
 
 function addEventListeners() {
   $("div#concerts-link").on("click", function(e) {
     e.preventDefault();
     $(".display").html("");
     $.get("/concerts", function(data) {
-      data.forEach(function(concertData) {
-        let newConcert = new Concert(concertData)
-        $(".display").append(newConcert.title + "<br>")
-      });
+      appendConcerts(data);
     });
   });
 
